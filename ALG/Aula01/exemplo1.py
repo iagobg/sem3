b0cd1d3fe8bd464257e26a35f396a2d7985d9d81
IAGO BUSSOLETTI GOMES <02456596099@senacrs.edu.br>
2025-03-17 10:26:19 -0300

    Algoritmos Aula 01

--- ALG/Aula01/exemplo1.py
+++ ALG/Aula01/exemplo1.py
@@ -1,3 +1,5 @@
+import math
+
 nome = input('Nome: ')
 idade = int(input('Idade: '))
 salario = float(input('Salário R$: '))
@@ -11,3 +13,27 @@ Varias Linhas
 print(f"Seu nome é {nome}")
 print(f'Sua idade é {idade} anos')
 print(f'Seu salário é de R$ {salario:9.2f}')
+
+
+if idade < 18:
+    print("Você é menor de idade")
+else:
+    print("Você é maior de idade")
+
+
+print('-'*20)
+
+
+bairro = input('Bairro: ').upper()
+
+match bairro:
+    case 'CENTRO':
+        print ('Você mora aqui perto...')
+    case 'FRAGATA' | 'TRES VENDAS':
+        print ('Ainda é perto')
+    case 'LARANJAL':
+        print('Você mora longe')
+    case _:
+        print('Não sei a distäância') 
+      
+    
---
+++ ALG/Aula01/exer.py
@@ -0,0 +1,54 @@
+import math
+
+while True:
+    choice = int(input('Número do exercicio: '))
+    if choice == 1:
+        pessoas = int(input('Número de pessoas: '))
+        peixes = int(input('Número de peixes: '))
+        print(f'Pagar R$: {pessoas*20+max(0,(peixes-pessoas)*12):.2f}')
+        
+    elif choice == 2:
+        numero = int(input('Número: '))
+        divisores = []
+        for x in range (1,numero):
+            if numero % x == 0:
+                divisores.append(x)
+        if sum(divisores) == numero:
+            print('O número é perfeito')
+        else:
+            print('O número não é perfeito')
+            
+    elif choice == 3:
+        produto = input('Produto: ')
+        etiquetas = int(input('No de Etiquetas: '))
+        while etiquetas > 0:
+            if etiquetas >= 2:
+                print(f'{produto}  {produto}')
+                etiquetas -= 2
+            else:
+                print(produto)
+                etiquetas -= 1
+                
+    elif choice == 4:
+        chinc = int(input('Número de chincilas: '))
+        anos = int(input('Anos de criação: '))
+        for x in range(0,anos):
+            print(f'{x+1} ano : {chinc} chinchilas')
+            chinc *= 3
+    elif choice == 5:
+        numeros = []
+        print('Informe números ou 0 para sair:')
+        while True:
+            num = int(input('Número: '))
+            if num == 0:
+                break
+            else:
+                numeros.append(num)
+        print('-'*10)
+        print(f'Números digitados: {len(numeros)}')
+        print(f'Soma dos números: {sum(numeros)}')
+        print(f'Maior número: {max(numeros)}')
+        
+    else:
+        print('Numero Invalido')
+        break
