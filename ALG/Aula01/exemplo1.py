f3a2b25a32fcce7b3c247a183c7cd0f56944dde7
IAGO BUSSOLETTI GOMES <02456596099@senacrs.edu.br>
2025-03-17 09:25:06 -0300

    First Commit

---
+++ ALG/Aula01/exemplo1.py
@@ -0,0 +1,13 @@
+nome = input('Nome: ')
+idade = int(input('Idade: '))
+salario = float(input('Salário R$: '))
+
+'''
+COmentario de
+Varias Linhas
+'''
+
+
+print(f"Seu nome é {nome}")
+print(f'Sua idade é {idade} anos')
+print(f'Seu salário é de R$ {salario:9.2f}')
