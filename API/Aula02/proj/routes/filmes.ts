16d531b8222e96bd424e4e77d2f84c682de28cae
IAGO BUSSOLETTI GOMES <02456596099@senacrs.edu.br>
2025-03-21 11:41:27 -0300

    API aula02

---
+++ API/Aula02/proj/routes/filmes.ts
@@ -0,0 +1,52 @@
+import {Router} from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const router = Router()
+const prisma = new PrismaClient()
+
+
+router.get('/',async(req,res) =>{
+    const filmes = await prisma.filme.findMany()
+    res.status(200).json(filmes)
+})
+
+router.post('/', async(req, res) => {
+    const { titulo, genero, duracao, preco, datacad, sinopse } = req.body
+
+    if( !titulo || !genero || !duracao || !preco){
+        res.status(400).json({erro:"Dados incompletos"})
+    }
+
+    const filme = await prisma.filme.create({
+        data: {titulo, genero, duracao, preco}
+    })
+
+    res.status(201).json(filme)
+})
+
+router.put('/:id', async(req,res) =>{
+    const {id} = req.params
+    
+})
+
+router.delete('/:id', async(req,res) => {
+    const { id } = req.params;
+
+    const filmealvo = await prisma.filme.findUnique({
+        where: {id: parseInt(id)}
+    });
+
+    // if (!filmealvo){
+    //     return res.status(404).json({erro: "Filme não encontrado"})
+    // }
+
+    await prisma.filme.delete({
+        where: {id: parseInt(id)}
+    });
+    res.status(200).json({mensagem: "Filme deletado com sucesso"});
+
+
+
+});
+
+export default router
