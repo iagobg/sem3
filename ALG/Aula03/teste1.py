7d52dd9a35eaf409381a717f4f26913edd8cbbf2
IagoBG <iago.bussoletti@gmail.com>
2025-04-16 16:50:56 -0300

    Gitignore rankings

--- ALG/Aula03/teste1.py
+++
@@ -1,6 +0,0 @@
-palavra = 'computador'
-letra = input('letra: ')
-if palavra.find(letra) >= 0:
-    print('Ok! Letra consta na palvara')
-else:
-    print('Erro, letra não consta.')
--- ALG/Aula03/tst.py
+++
@@ -1,8 +0,0 @@
-import math
-
-txt = 'iago.bussoletti@gmail.com'
-
-email=txt.split('@')
-
-
-print(email[1])
